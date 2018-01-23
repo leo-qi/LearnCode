86520488f43bf8f91b9fd826827482b37441db35
Leo Qi <qilei101@gmail.com>
2018-01-23 15:32:29 +0800

    第七章 Web开发:集成在一起

--- Python/Head_First_Python/Chapter7/AthleteList.py
+++ Python/Head_First_Python/Chapter7/AthleteList.py
@@ -1,12 +1,12 @@
 class AthleteList(list):
     def __init__(self, a_name, a_dob=None, a_times=[]):
-        list.__init__(a_times)
+        list.__init__([])
         self.name = a_name
         self.dob = a_dob
-        self.time = a_times
+        self.extend(a_times)
     
     def top3(self):
-        return sorted(set([sanitize(t) for t in self.time]))[0:3]
+        return sorted(set([sanitize(t) for t in self]))[0:3]
 
 def sanitize(time_string):
     if '-' in time_string:
