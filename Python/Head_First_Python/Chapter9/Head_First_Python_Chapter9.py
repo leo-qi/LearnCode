c766dca688505fd27ea7ccf97e76183b3d933cc7
Leo Qi <qilei101@gmail.com>
2018-01-30 23:08:26 +0800

    第九章 管理你的数据:输入处理

--- Python/Head_First_Python/Chapter9/Head_First_Python_Chapter9.py
+++ Python/Head_First_Python/Chapter9/Head_First_Python_Chapter9.py
@@ -2,9 +2,52 @@
 第九章 管理你的数据:输入处理
 '''
 
-## TODO
+'''
+利用Python的数据库API
+1、连接：建立与数据库的连接
+2、创建：创建一个游标，通过连接与数据通信
+3、交互：利用游标，是用SQL管理数据
+4、提交\回滚
+5、关闭：撤销与数据库后台的连接（游标也会撤销）
+'''
+import sqlite3
+
+connection = sqlite3.connect('test.sqlite')
+
+cursor = connection.cursor()
+
+cursor.execute("""SELECT DATE('NOW')""")
+# 大多数数据库系统并不需要提交，不过SQLLite要求提交
+connection.commit()
+
+connection.close()
 
 '''
-总结:
+coachdata.sqlite
+create table athletes (
+    id integer primary key autoincrement unique not null,
+    name text not null,
+    dob date not null
+)
 
+create table timing_data(
+    athlete_id integer not null,
+    value text not null,
+    foreign key (athlete_id) references athletes
+)
 '''
+
+connection = sqlite3.connect('cocahdata.sqlite')
+cursor = connection.cursor()
+cursor.execute("""CREATE TABLE athletes (
+    id integer primary key autoincrement unique not null,
+    name text not null,
+    dob date not null)""")
+
+#cursor.execute("INSERT INTO athletes (name, dob) VALUES (?, ?)", (name, dob))
+cursor.execute("""CREATE TABLE timing_data (
+    athlete_id integer not null,
+    value text not null,
+    foreign key (athlete_id) references athletes)""")
+connection.commit()
+connection.close()
--- Python/Head_First_Python/Chapter9/webapp/cgi-bin/add_timing_data.py
+++ Python/Head_First_Python/Chapter9/webapp/cgi-bin/add_timing_data.py
@@ -7,10 +7,12 @@ import yate
 print(yate.start_respone('text/plain'))
 addr = os.environ['REMOTE_ADDR']
 host = os.environ['REMOTE_HOST']
-mothed = os.environ['REMOTE_METHOD']
+mothed = os.environ['REQUEST_METHOD']
 cur_time = time.asctime(time.localtime())
-print(host + ", " + addr + ", " + cur_time + ": " mothed + ": ", end='', file=sys.stderr)
+print(host + ', ' + addr + ', ' + cur_time + ': ' + mothed + ': ', end='', file=sys.stderr)
 
 form = cgi.FieldStorage()
 for each_form_item in form.keys():
-    
+    print(each_form_item + '->' + form[each_form_item].value, end='', file=sys.stderr)
+print(file=sys.stderr)
+print('OK.')
